888b89d5343607dc93ba365f2413c5c3b3d5f38e
Matty <mateuszbuczak1@gmail.com>
2026-06-17 23:04:05 +0200

    fix

--- .next/static/chunks/app_(default)_page_tsx_51be82bf._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/components_d7481b03._.js",
-  "static/chunks/node_modules_next_8178feb9._.js"
-],
-    source: "dynamic"
-});
