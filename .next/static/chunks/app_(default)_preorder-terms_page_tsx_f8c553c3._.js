c0ebf6920945b79500d31cf33e2e88f2ce0ea158
Matty <mateuszbuczak1@gmail.com>
2026-06-20 19:23:49 +0200

    filled out documents

---
+++ .next/static/chunks/app_(default)_preorder-terms_page_tsx_f8c553c3._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/app_(default)_preorder-terms_page_tsx_694a8ef2._.js"
+],
+    source: "dynamic"
+});
