72bf1cd8938ff5a2ac16881a78e88af865be5012
Matty <mateuszbuczak1@gmail.com>
2026-06-20 19:36:24 +0200

    fixes

--- .next/static/chunks/app_(default)_preorder-terms_page_tsx_f8c553c3._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/app_(default)_preorder-terms_page_tsx_694a8ef2._.js"
-],
-    source: "dynamic"
-});
