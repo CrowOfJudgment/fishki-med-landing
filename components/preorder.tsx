5445f2cdb94070e5bda87b796cceac2db50941a8
Matty <mateuszbuczak1@gmail.com>
2026-06-24 23:13:25 +0200

    removed unnecessary checkbox

--- components/preorder.tsx
+++ components/preorder.tsx
@@ -193,13 +193,6 @@ export default function Preorder() {
                     </span>
                   </label>
 
-                  <label className="flex cursor-pointer items-start gap-3 rounded-[1.15rem] border border-[#B9DDD5] bg-white p-4 text-sm leading-6 text-[#274D53]">
-                    <input
-                      type="checkbox"
-                      className="mt-1 h-4 w-4 shrink-0 accent-[#0F766E]"
-                    />
-                    <span>{t.preorder.marketingConsent}</span>
-                  </label>
                 </div>
 
                 <button
